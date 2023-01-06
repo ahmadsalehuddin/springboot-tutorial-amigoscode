fe18ab39c462053b3b4863ec7aaba7469c2ddb87
ahmadsalehuddin <ahmadsalehuddinazzizi@gmail.com>
2023-01-07 01:22:18 +0800

    add addNewStudent method

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,6 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,7 +9,19 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudent() {
-        return List.of(new Student(1L, "Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21));
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
     }
 }
